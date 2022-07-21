202f39d58116941a3a824adf8b2c74ab95891951
Matt Neal <neal.matt.m@gmail.com>
2022-07-20 23:22:18 -0400

    finished project

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const licenses = ['Apache 2.0 License'
+                 ,'BSD 2-Clause License'
+                 ,'GNU General Public License v2'
+                 ,'MIT License']
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {type: 'input', name: 'title', message: 'Enter project title'}
+                  , {type: 'input', name: 'description', message: 'Enter description of project'}
+                  , {type: 'input', name: 'installation', message: 'Enter installation instructions'}
+                  , {type: 'input', name: 'usage', message: 'Enter usage information'}
+                  , {type: 'input', name: 'contributing', message: 'Enter contribution guidelines'}
+                  , {type: 'input', name: 'tests', message: 'Enter test instructions'}
+                  , {type: 'list',  name: 'license', message: 'Choose license', choices: licenses}
+                  , {type: 'input', name: 'username', message: 'Enter GitHub username'}
+                  , {type: 'input', name: 'email', message: 'Enter email address'} ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => { if(err) throw err })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => writeToFile('./README.md', generateMarkdown(answers)))
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'Apache 2.0 License':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    case 'BSD 2-Clause License':
+      return '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)'
+    case 'GNU General Public License v2':
+      return '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)'
+    case 'MIT License':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    default:
+      return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +25,22 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}`
+   + `\n${renderLicenseBadge(data.license)}`
+   + `\n\n## Description\n${data.description}`
+   + `\n\n## Table of Contents`
+     + `\n- [Installation](#installation)`
+     + `\n- [Usage](#usage)`
+     + `\n- [License](#license)`
+     + `\n- [Contributing](#contributing)`
+     + `\n- [Tests](#tests)`
+     + `\n- [Questions](#questions)`
+   + `\n\n## Installation\n${data.installation}`
+   + `\n\n## Usage\n${data.usage}`
+   + `\n\n## License\nThis project is covered under the ${data.license}`
+   + `\n\n## Contributing\n${data.contributing}`
+   + `\n\n## Tests\n${data.tests}`
+   + `\n\n## Questions\nI can be reached on [GitHub](https://www.github.com/${data.username}/) or at ${data.email}.`
 }
 
 module.exports = generateMarkdown;
